66ed8d5c7a1f9b445cb5e482ea5350939f87c686
Lisa Sun <sunlisa@gmail.com>
2022-08-01 16:09:16 -0500

    add openweather fetch

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 import '../services/location.dart';
 
@@ -40,6 +41,17 @@ class _LoadingScreenState extends State<LoadingScreen> {
     }
   }
 
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // try-catch example. if try fails, then catch renders with default value
@@ -56,7 +68,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     //     color: Colors.red,
     //   ),
     // );
-
+    getData();
     return Scaffold();
   }
 }
