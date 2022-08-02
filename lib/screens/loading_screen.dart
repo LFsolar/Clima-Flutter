8746a8ba8a284298dc6f1d6666d36b611e2a95e4
Lisa Sun <sunlisa@gmail.com>
2022-08-01 21:55:30 -0500

    add json parsing

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert'; // for decoding json
 
 import '../services/location.dart';
 
@@ -46,7 +47,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+
+      var decodedData = jsonDecode(data);
+      int weatherId = decodedData['weather'][0]['id'];
+      print('weatherId: ' + weatherId.toString());
+
+      double temp = decodedData['main']['temp'];
+      print('temp: ' + temp.toString());
+
+      String cityName = decodedData['name'];
+      print('cityName: ' + cityName);
     } else {
       print(response.statusCode);
     }
